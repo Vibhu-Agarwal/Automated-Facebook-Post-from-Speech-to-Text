1ca9ba5e9a662e7e681b55ce2a8c07e0f138ba04
Vibhu Agarwal <vibhu4agarwal@gmail.com>
2018-03-10 14:14:29 +0530

    Python script for speech to text conversion

---
+++ text_to_speech.py
@@ -0,0 +1,40 @@
+import requests
+import json
+from Record_post import record_audio, read_audio
+
+# Wit speech API endpoint
+API_ENDPOINT = 'https://api.wit.ai/speech'
+
+# Wit.ai api access token
+wit_access_token = 'PSHMN5CW5LEKIU5NTJIGPTNPHHT3FIFO' #'XXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+
+
+def RecognizeSpeech(AUDIO_FILENAME, num_seconds = 5):
+    
+    # record audio of specified length in specified audio file
+    record_audio(num_seconds, AUDIO_FILENAME)
+    
+    # reading audio
+    audio = read_audio(AUDIO_FILENAME)
+    
+    # defining headers for HTTP request
+    headers = {'authorization': 'Bearer ' + wit_access_token,
+               'Content-Type': 'audio/wav'}
+
+    # making an HTTP post request
+    resp = requests.post(API_ENDPOINT, headers = headers,
+                         data = audio)
+    
+    # converting response content to JSON format
+    data = json.loads(resp.content)
+    
+    # get text from data
+    text = data['_text']
+    
+    # return the text
+    return text
+
+
+if __name__ == "__main__":
+    text =  RecognizeSpeech('myspeech.wav', 5)
+    print("\nYou said: {}".format(text))
